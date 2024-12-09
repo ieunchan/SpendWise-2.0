7b8b265263ea042cff7f1e40b9b4ca7202b7457a
이은찬 <jsmisth26@gmail.com>
2024-12-09 20:09:57 +0900

    fix: streamlit 배포를 위한 CORS 추가

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import time
 from app.routes import expense, income, others
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
+
+# CORS 설정
+origins = [
+    "https://spendwise.streamlit.app",  # Streamlit 배포 URL
+    "https://d792-124-60-151-80.ngrok-free.app",  # Ngrok URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 허용할 출처
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용 (GET, POST 등)
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
 
 # 응답 시간 측정 미들웨어
 @app.middleware("http")
@@ -12,7 +27,7 @@ async def add_process_time_header(request: Request, call_next):
     response = await call_next(request)  # 요청 처리
     end_time = time.perf_counter()  # 요청 완료 시간
     process_time = end_time - start_time  # 처리 시간 계산
-    response.headers["X-Process-Time"] = f"{process_time:.4f}"  # 응답 헤더에 추가
+    response.headers["X-Process-Time"] = f"{process_time:.4f}"  # 응답 헤더에 처리 시간 추가
     return response
 
 # 라우터 등록
